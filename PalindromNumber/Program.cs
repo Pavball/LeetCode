373e10a961c23d831191a12f5c02f97da94931a8
Marco Matijević <marco.matijevic2002@gmail.com>
2024-04-10 19:54:02 +0200

    Initial commit

---
+++ PalindromNumber/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromNumber
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool isPalX;
+            bool isPalY;
+            int x = 10;
+            int y = -121;
+
+            isPalX = IsPalindrome(x);
+            isPalY = IsPalindrome(y);
+
+            Console.WriteLine(isPalX);
+            Console.WriteLine(isPalY);
+
+        }
+        public static bool IsPalindrome(int x)
+        {
+            bool isPal = false;
+
+            string word = x.ToString();
+
+            Console.WriteLine(word);
+
+            //Reversing string
+            char[] stringArray = word.ToCharArray();
+            Array.Reverse(stringArray);
+            string reversedStr = new string(stringArray);
+
+            Console.WriteLine(reversedStr);
+
+            //Checking if same
+            if (word.Equals(reversedStr)) isPal = true;
+
+            return isPal;
+        }
+
+
+    }
+
+
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numIdx = new int[2];
+
+            //1st
+            //int[] nums = [2, 7, 11, 15];
+            //int target = 9;
+
+            int[] nums = [3, 3];
+            int target = 6;
+
+            numIdx = TwoSumProblem(nums, target);
+
+            foreach (var item in numIdx)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        public static int[] TwoSumProblem(int[] nums, int target)
+        {
+            int[] numberIdx = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (target == (nums[i] + nums[j]))
+                    {
+                        numberIdx[0] = i;
+                        numberIdx[1] = j;
+                        return numberIdx;
+                    }
+                }
+            }
+
+            return numberIdx;
+        }
+    }
+}
