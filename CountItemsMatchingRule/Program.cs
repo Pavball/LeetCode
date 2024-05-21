50d5f5f3bda017c897d927c675264475155dc6f9
Marco Matijević <marco.matijevic2002@gmail.com>
2024-05-21 20:14:51 +0200

    Easy

---
+++ CountItemsMatchingRule/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountItemsMatchingRule
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IList<IList<string>> items = [["phone", "blue", "pixel"], ["computer", "silver", "phone"], ["phone", "gold", "iphone"]];
+
+            string ruleKey = "type";
+            string ruleValue = "phone";
+
+            int count = CountMatches(items, ruleKey, ruleValue);
+
+            Console.WriteLine(count);
+
+        }
+
+        public static int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+        {
+            int count = 0;
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                
+            switch (ruleKey)
+            {
+                case "type":
+
+                            if (items[i][0].Equals(ruleValue))
+                            {
+                                count++;
+                            }
+                        break;
+                    case "color":
+                            if (items[i][1].Equals(ruleValue))
+                            {
+                                count++;
+                            }
+                        break;
+                case "name":
+                            if (items[i][2].Equals(ruleValue))
+                            {
+                                count++;
+                            }
+                        break;
+                default:
+                    // code block
+                    break;
+            }
+
+            }
+
+
+            return count;
+        }
+    }
+}
---
+++ ScoreOfAString/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreOfAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "hello";
+
+            int score = ScoreOfString(s);
+
+            Console.WriteLine(score);
+        }
+
+        public static int ScoreOfString(string s)
+        {
+            int score = 0;
+
+            List<char> list = new List<char>();
+           
+
+            list = s.ToCharArray().ToList();
+
+            int[] stringValueArray = new int[list.Count];
+
+            for (int i = 0; i < stringValueArray.Length; i++)
+            {
+                stringValueArray[i] = System.Convert.ToInt32(list[i]);
+                Console.WriteLine(i + ": " + stringValueArray[i] + " Letter: " + list[i]);  
+            }
+
+            Console.WriteLine("");
+            int helpIdx = 0;
+
+            for (int i = 1; i < stringValueArray.Length; i++)
+            {
+                helpIdx = i;
+                score += Math.Abs(stringValueArray[helpIdx - 1] - stringValueArray[i]);
+                Console.WriteLine("Current score: " + score);
+            }
+
+
+
+            return score;
+        }
+    }
+}
